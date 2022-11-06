b85cc187d1464708c2a29a4ee30f876ccdad9b65
Emmanuel Campos <ejcampos08@gmail.com>
2022-11-06 20:44:26 +0800

    feat: display users

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from "firebase/auth"
-import { doc, getDoc } from "firebase/firestore"
+import { doc, getDoc, getDocs, collection  } from "firebase/firestore"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { auth, db } from "../lib/firebase"
@@ -7,25 +7,30 @@ import { auth, db } from "../lib/firebase"
 const Home = () => {
     const router = useRouter()
     const [user, setUser] = useState(null)
-    const currentUser = auth.currentUser
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
         (async () => {
-            if (currentUser) {
-                const ref = doc(db, "users", currentUser.uid)
+            const usersRef = await getDocs(collection(db, "users"))
+            const usersSnap = usersRef.docs.map(u => u.data())
+            setUsers(usersSnap)
+        })()
+
+        onAuthStateChanged(auth, async user => {
+            if (user) {
+                const ref = doc(db, "users", user.uid)
                 const snap = await getDoc(ref)
                 setUser(snap.data())
             }
-        })()
-        
-    }, [currentUser])
+        })
+    }, [])
 
     const logout = () => {
         auth.signOut()
         router.push('/')
     }
 
-    if (!currentUser) {
+    if (!user) {
         return (
             <>
                 <h1>User not logged in</h1>
@@ -45,6 +50,26 @@ const Home = () => {
                 </ul>
                 <button type="button" onClick={() => logout()}>Logout</button>
             </div>
+            <hr />
+            <h3>Users</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Fave animal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { users?.map((u, i) => (
+                        <tr key={i}>
+                            <td>{u.name}</td>
+                            <td>{u.address}</td>
+                            <td>{u.fave}</td>
+                        </tr>
+                    )) }
+                </tbody>
+            </table>
         </div>
     )
 }
